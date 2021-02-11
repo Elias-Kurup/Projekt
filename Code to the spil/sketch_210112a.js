026a900e0608ff3116325b0d1a4d2eb50eb08b42
Elias-Kurup <77321881+Elias-Kurup@users.noreply.github.com>
2021-02-11 20:19:49 +0100

    Update sketch_210112a.js

--- Code to the spil/sketch_210112a.js
+++ Code to the spil/sketch_210112a.js
@@ -9,15 +9,25 @@ var frame;
 //the scene is twice the size of the canvas
 var SCENE_W = 1600;
 var SCENE_H = 800;
+var enemy;
 
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(1600, 800);
+  collectibles = new Group();
+  
+  for(var j=0; j<10; j++)
+  {
+    var dot = createSprite(random(0, width), random(0, height));
+    dot.addAnimation('normal', 'assets/small_circle0001.png', 'assets/small_circle0001.png');
+    collectibles.add(dot);
+  }
+
 
   //create a sprite and add the 3 animations
   ghost = createSprite(400, 200, 50, 100);
 
-  var myAnimation = ghost.addAnimation('floating', 'assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+  var myAnimation = ghost.addAnimation('floating', 'assets/ghost_standing0002.png', 'assets/ghost_standing0007.png');
   myAnimation.offY = 18;
 
   ghost.addAnimation('moving', 'assets/ghost_walk0001.png', 'assets/ghost_walk0004.png');
@@ -34,7 +44,7 @@ function setup() {
     bg.add(rock);
   }
 
-  frame = loadImage('assets/frame.png');
+  frame = loadImage();
 }
 
 function draw() {
@@ -47,10 +57,7 @@ function draw() {
   //a camera is created automatically at the beginning
 
   //.5 zoom is zooming out (50% of the normal size)
-  if(mouseIsPressed)
-    camera.zoom = 0.5;
-  else
-    camera.zoom = 1;
+
 
   //set the camera position to the ghost position
   camera.position.x = ghost.position.x;
@@ -77,10 +84,29 @@ function draw() {
   ellipse(ghost.position.x, ghost.position.y+90, 80, 30);
   //character on the top
   drawSprite(ghost);
-
+    drawSprites();
   //I can turn on and off the camera at any point to restore
   //the normal drawing coordinates, the frame will be drawn at
   //the absolute 0,0 (try to see what happens if you don't turn it off
   camera.off();
   image(frame, 0, 0);
+
 }
+
+
+function mousePressed() {
+
+  //create a sprite at the mouse position and store it in a temporary variable
+  var s = createSprite(ghost.position.x, ghost.position.y, 30, 30);
+  //if no image or animation is associated it will be a rectancle of the specified size
+  //and a random color
+
+  //now you can use the variable to set properties
+  //e.g. a random velocity on the x and y coordinates
+  s.velocity.x = (camera.mouseX-ghost.position.x)/2;
+  s.velocity.y = (camera.mouseY-ghost.position.y)/2;
+}
+  collectibles.attractionPoint(0.2, ghost.position.x, ghost.position.y);
+  //since the force keeps incrementing the speed you can
+  //set a limit to it with maxSpeed
+  collectibles.maxSpeed = 5;
